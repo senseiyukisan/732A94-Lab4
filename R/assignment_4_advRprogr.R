9b622ef56ca78f4982cca496a1c75959b6dc5245
nicta839 <nicolas.juha.taba@gmail.com>
2020-09-15 13:36:40 +0200

    assignment progress

--- R/assignment_4_advRprogr.R
+++ R/assignment_4_advRprogr.R
@@ -1,5 +1,54 @@
+linreg = function(formula, data) {
+  X <- model.matrix(formula, data) #What is this doing exactly?
+  y <- as.matrix(data[all.vars(formula)[1]]) #this gives the right answer but I don't know why
+  # y <- as.matrix(data[all.vars(formula)])
+  # print(y)
+  # print(class <- class(y))
+  # print(attributes <- attributes(y))
+  # print(type_of <- typeof(y))
+  # 
+  #Regression coefficients
+  Beta <- solve(t(X) %*% X) %*% t(X) %*% y
+
+  #Fitted values
+  fit_val <- X %*% Beta
+
+  #Residuals
+  e <- y - fit_val
+
+  #Degrees of freedom
+  n <- nrow(X)
+  p <- ncol(X)
+  df <- n-p
+
+  #Residual variance
+  sigma2 <- (t(e)%*%e)/df
+  
+  # #variance of regression coefficients
+  var_beta <- solve(t(X) %*% X) * sigma2[1]
+  # print(Beta)
+  # print(class <- class(Beta))
+  # print(attributes <- attributes(Beta))
+  # print(type_of <- typeof(Beta))
+
+  #t-values for each coefficient
+  t <- Beta %*% 1/sqrt(abs(var_beta)) #wrong size of table
+ 
+  # #Return
+  # return(linreg1(formula <- formula,
+  #               data <- data,
+  #               regression_coefficients <- Beta,
+  #               fitted_values <- fit_val,
+  #               residuals <- e,
+  #               degrees_freedom <- df,
+  #               residual_variance <- sigma2,
+  #               variance_regression_coefficicents <- var_beta,
+  #               t_values <- t))
+}
+
+
 # Create object with fields and methods
-linreg <- setRefClass("linreg",
+linreg1 <- setRefClass("linreg1",
                        fields = list(formula = "formula",
                                      data = "data.frame",
                                      regression_coefficients = "vector",
@@ -10,62 +59,24 @@ linreg <- setRefClass("linreg",
                                      variance_regression_coefficients = "matrix",
                                      t_values = "vector"),
                        methods = list(
-                         init = function(formula, data) {
-                           X <- model.matrix(formula, data)
-                           y <- all.vars(formula[1])
-                           linreg$formula <- formula
-                           linreg$data.frame <- data
-
-                           #Regression coefficients
-                           Beta <- solve(t(X) %*% X) %*% t(X) %*% y
-                           linreg$regression_coefficients <- Beta
-                           
-                           #Fitted values
-                           fit_val <- X %*% Beta
-                           linreg$fitted_values <- fit_val
-                           
-                           #Residuals
-                           e <- y - fit_val
-                           linreg$residuals <- e
-                           
-                           #Degrees of freedom
-                           n <- nrow(X)
-                           p <- ncol(X)
-                           df <- n-p
-                           linreg$degrees_freedom <- df
-                           
-                           #Residual variance
-                           sigma2 <- (t(e)%*%e)/df
-                           linreg$residual_variance <- sigma2
-                           
-                           #variance of regression coefficients
-                           var_beta <- sigma2 * solve(t(X) %*% X)
-                           linreg$variance_regression_coefficicents <- var_beta
-                           
-                           #t-values for each coefficient
-                           t <- Beta/sqrt(var_beta)
-                           linreg$t_values <- t
-                           
-                           #p-values
-                           p_values <- pt(Beta)
-                         },
-                         # },
+                         
                          # print = function(){
                          #   cat(paste0("Formula: ", .self$formula, "data: ", .self$data, "\n"))
                          #   },
                          resid <- function(){
-                           return(e)
+                           return(.self$residuals)
                          }
-                         # },
-                         # pred <- function(){
-                         #   return(fit_val)
-                         # },
-                         # coef <- function(){
-                         #   Beta_named <- Beta
-                         #   for(i in 1:length(Beta_named)){
-                         #     names(Beta_named)[i] <- cat("Coefficient ", as.character(i))
-                         #   }
-                         #   return(Beta_named)
-                         # }
+                         },
+                         pred <- function(){
+                           return(.self$fitted_values)
+                         },
+                         coef <- function(){
+                           Beta_named <- .self$regression_coefficients
+                           for(i in 1:length(Beta_named)){
+                             names(Beta_named)[i] <- cat("Coefficient ", as.character(i))
+                           }
+                           return(Beta_named)
+                         }
                        )
 )
+
