9bdfd376b673a34f0e30e6005ebe22204504bc95
Yuki <yuki@Yukis-MacBook-Pro-2.local>
2020-09-15 14:47:33 +0200

    added package R file

---
+++ R/linearregression.R
@@ -0,0 +1,10 @@
+#' linearregression: A package that lets you calculate a linear regression 
+#'
+#' The linearregression package provides 1 categories of important functions:
+#' linreg
+#' 
+#' @section nicolasyuki functions:
+#' linreg
+#'
+#' @docType package
+#' @name linearregression
